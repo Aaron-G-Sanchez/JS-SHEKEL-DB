10cc3bc6e46a6225f9034f2d22bee3152f7cc1bf
Aaron Sanchez <saarong12@gmail.com>
2023-12-25 04:39:34 -0600

    Added first unit test to check that seed files creates users

---
+++ __tests__/user.test.js
@@ -0,0 +1,18 @@
+const { describe, test, expect, beforeAll } = require('@jest/globals') 
+
+const { User } = require('../src/models')
+const { seedSync } = require('../db/seed')
+
+beforeAll(async () => {
+    await seedSync()
+})
+
+describe('User model tests', () => {
+    test('Should read all the users in the db', async() => {
+
+    const userArr = await User.findAll()
+
+    expect(Array.isArray(userArr)).toBe(true)
+    expect(userArr[0]).toBeInstanceOf(User)
+    })
+})
