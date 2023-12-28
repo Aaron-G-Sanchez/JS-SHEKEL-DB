8399858c2658a544a5a54fb8d544b71da1f804fe
Aaron Sanchez <saarong12@gmail.com>
2023-12-28 05:01:16 -0600

    Created a route to get user by username

--- src/router/user.js
+++ src/router/user.js
@@ -17,6 +17,26 @@ userRouter.get('/', async (req, res, next) => {
   }
 })
 
+userRouter.get('/:username', async(req, res, next) => {
+  const username = req.params
+
+  try {
+    const user = await User.findAll({
+      where: {
+        userName: username.username
+      }
+    })
+
+      if (!user) {
+        throw new error(`No user by username: ${username}`)
+      }
+
+      res.send(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
 module.exports = {
   userRouter
 }
